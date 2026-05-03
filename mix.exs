46db3e214c9508ab44289415ff71ea43ca0e070d
Neni <felipe.dacs@gmail.com>
2026-05-03 18:59:45 -0300

    :zzz: muda versão do elixir (1.18 + otp 26)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bazar.MixProject do
     [
       app: :bazar,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
